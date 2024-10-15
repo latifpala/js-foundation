7be1cde5af6211694e580cade720c213186e0732
Latif Pala <latifpala@gmail.com>
2024-10-15 07:23:13 +0000

    01_varibles final

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,11 @@
-/* const accountId = 123456;
+const accountId = 123456;
 let accountEmail = "latif@gmail.com";
 var accountPassword = "12345";
 
 //accountId = 123123 // not allowed to change value of constants
 
 accountEmail = "latifpala@gmail.com";
-accountCity = "Rajkot";
+accountCity = "Rajkot"; // we can define variables like this but its not good practise
 
 console.log(accountCity);
-//console.table([accountId, accountEmail, accountPassword, accountCity]); */
-console.log("test");
+console.table([accountId, accountEmail, accountPassword, accountCity]); 
