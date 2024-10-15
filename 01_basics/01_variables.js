d26cff5d0317475c1bcf653b33c313701934a3b8
Latif Pala <latifpala@gmail.com>
2024-10-15 07:17:39 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+/* const accountId = 123456;
+let accountEmail = "latif@gmail.com";
+var accountPassword = "12345";
+
+//accountId = 123123 // not allowed to change value of constants
+
+accountEmail = "latifpala@gmail.com";
+accountCity = "Rajkot";
+
+console.log(accountCity);
+//console.table([accountId, accountEmail, accountPassword, accountCity]); */
+console.log("test");
