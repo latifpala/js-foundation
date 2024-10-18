649413f7ce8bead45760d0c9a4c541a4d199556b
Latif Pala <latifpala@gmail.com>
2024-10-18 07:14:29 +0000

    Datatypes and Dataconversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+// "33" = 33
+// 33abc = NaN
+// true = 1; false = 0
+
+let isLoggedIn = "latif"
+//console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "latif" => true
+
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
