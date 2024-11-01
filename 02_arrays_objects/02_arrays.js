9e56586cf8845286995f4f7c7ea55f976f8688ca
Latif Pala <latifpala@gmail.com>
2024-11-01 07:39:35 +0000

    Arrays and objects

---
+++ 02_arrays_objects/02_arrays.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros) // pushes entire array instead of array elements
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros] // spread operator is used to spread array elements
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+ 
+console.log(Array.isArray("Hitesh")) // false because its a string
+console.log(Array.from("Hitesh")) // converts each character to individual array element
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
