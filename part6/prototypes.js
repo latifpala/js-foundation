cd0ed61b448dd4f43d5ec9b0d989328e49dbafac
Latif Pala <latifpala@gmail.com>
2025-12-25 06:13:32 +0000

    Prototypes

---
+++ part6/prototypes.js
@@ -0,0 +1,16 @@
+let computer = {cpu : 12};
+let lenovo = {screen : 'HD', __proto__ : computer};
+let tomHardware = {};
+
+//console.log(lenovo.__proto__);
+
+let genericCar = { tyres: 4 };
+
+let tesla = {
+  driver: "AI",
+};
+
+Object.setPrototypeOf(tesla, genericCar);
+
+console.log(`tesla `, Object.getPrototypeOf(tesla));
+
